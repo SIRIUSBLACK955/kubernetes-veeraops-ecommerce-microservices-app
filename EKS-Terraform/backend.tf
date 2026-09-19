31cd7fadf80de154f5293d6bc2de94cd279f16a2
SIRIUSBLACK955 <dasdebasis250@gmail.com>
2026-09-19 14:20:19 +0530

    Change S3 bucket name for Terraform backend

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "siriusblack955250720test"
+    bucket       = "siriusblack955250720dev"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
